b229b046d041e26683bbac8c091c33c09f8ecedb
Ivan <reinaldoivan777@gmail.com>
2020-02-24 16:35:40 +0700

    update styles and update linter rules

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <h1 class="mb-3">Search Github User's Repositories</h1>
-    <form @submit.prevent="getRepository">
-      <input type="text" class="input-text mb-3" placeholder="Github Username" v-model="inputName" />
-      <button type="submit" class="btn btn-primary">Get Repositories</button>
-    </form>
-    <Result v-if="!loading" :repos="repos" :username="username" />
-    <b-spinner v-else type="grow" variant="primary" label="Spinning" class="my-3"></b-spinner>
-    <h5 v-if="notFound" class="my-3">Result Not Found!</h5>
+    <div class="search">
+      <h1 class="mb-3">Search Github User's Repositories</h1>
+      <form @submit.prevent="getRepository">
+        <input v-model="inputName" type="text" class="input-text mb-3" placeholder="Github Username" />
+        <button type="submit" class="btn btn-primary">Get Repositories</button>
+      </form>
+    </div>
+    <div class="content">
+      <Result v-if="!loading" :repos="repos" :username="username" />
+      <b-spinner v-else type="grow" variant="primary" label="Spinning" class="my-3"></b-spinner>
+      <h5 v-if="notFound" class="my-3">Result Not Found!</h5>
+    </div>
   </div>
 </template>
 
@@ -62,5 +66,50 @@ export default {
   transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
   transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out, -webkit-box-shadow 0.15s ease-in-out;
   margin-right: 1rem;
+}
+
+.search {
+  position: fixed;
+  width: 100%;
+  border-bottom: 1px solid rgb(198, 198, 198);
+  background-color: #ffffff;
+  z-index: 800;
+  top: 0;
+  padding: 16px 0;
+}
+
+.content {
+  position: relative;
+  top: 160px;
+}
+
+@media only screen and (max-width: 320px) {
+  .content {
+    top: 200px;
+  }
+
+  h1 {
+    font-size: 1.5rem !important;
+  }
+}
+
+@media only screen and (min-width: 321px) and (max-width: 400px) {
+  .content {
+    top: 175px;
+  }
+
+  h1 {
+    font-size: 1.5rem !important;
+  }
+}
+
+@media only screen and (min-width: 401px) and (max-width: 500px) {
+  .content {
+    top: 135px;
+  }
+
+  h1 {
+    font-size: 1.5rem !important;
+  }
 }
 </style>
